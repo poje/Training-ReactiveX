1e4e4240573eb7495846029ab6c74cc6c894ac29
Jorge Villaseca R <jorge.villaseca91@gmail.com>
2021-12-10 15:04:25 -0300

    Se agrega ejemplo de pluck

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { range, fromEvent } from 'rxjs';
-import { map } from 'rxjs/operators'
+import { map, pluck } from 'rxjs/operators'
 
 // range(1,5).pipe(
 //     map<number,string>(val => (val * 10).toString())
@@ -13,4 +13,12 @@ const keyupCode$ = keyup$.pipe(
     map(event => event.code)
 )
 
+const keyupPluck$ = keyup$.pipe(
+    pluck('key')
+);
+
+
+keyup$.subscribe( console.log )
+
 keyupCode$.subscribe( code => console.log('map', code));
+keyupPluck$.subscribe( code => console.log('pluck', code));
